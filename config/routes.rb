53e61193355048af97f5fe8cb21ea15dccc4cb90
kuromu0104 <nccjb457@yahoo.co.jp>
2020-07-07 11:49:56 +0900

    edit-route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+    #root "items#index"
+  #get "toppage/index"
+  # root "toppage#index"
+  #resources :items do
+    #collection do
+      #get 'search'
+    #end
+  resources :users
+  #end
 end
