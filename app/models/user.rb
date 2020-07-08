988b19577174a0eb0793d7ccec42d0f9bf7e80d7
kuromu0104 <nccjb457@yahoo.co.jp>
2020-07-08 12:11:43 +0900

    新規登録機能emailの一意性制約追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :email, uniqueness: true
+
   has_one :profile, dependent: :destroy
   has_one :sending_destination, dependent: :destroy
   accepts_nested_attributes_for :profile
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_013208) do
+ActiveRecord::Schema.define(version: 2020_07_08_023259) do
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", null: false
