ed4e43179e3e4360a265d3a9352617a0f5294851
eri-Practice <64237498+eri-Practice@users.noreply.github.com>
2020-07-08 14:24:28 +0900

    itemテーブルに保存できるようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new 
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to new_item_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :text, :price, :category, :condition, :postage_payer, :prefecture, :standby_day, :trading_status, :seller, :buyer)
+    # .merge(seller: current_user.id) ログイン機能実装後に反映可能
+  end
+
 end
