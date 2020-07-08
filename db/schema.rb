66563ea1f5b6cb04c6b21aebb0585caaf6182a94
kuromu0104 <nccjb457@yahoo.co.jp>
2020-07-08 10:31:33 +0900

    profileテーブルnull falseを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_075055) do
+ActiveRecord::Schema.define(version: 2020_07_08_012410) do
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "first_name"
-    t.string "family_name"
-    t.string "first_name_kana"
-    t.string "family_name_kana"
-    t.date "birth_date"
+    t.string "first_name", null: false
+    t.string "family_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "family_name_kana", null: false
+    t.date "birth_date", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
