3f43b38af640cd2743eb5016b7cbaec07c49e073
eri-Practice <64237498+eri-Practice@users.noreply.github.com>
2020-07-14 12:47:24 +0900

    編集画面の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit]
+  before_action :move_to_index, only: [:edit]
+
   def index
   end
 
@@ -10,10 +13,22 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to root_path, notice: '商品を出品しました'
     else
       @item.images.new
       render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path, notice: '商品を編集しました'
+    else
+      render :edit
     end
   end
 
@@ -22,4 +37,12 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :text, :price, :category, :condition, :postage_payer, :prefecture_id, :standby_day, :trading_status, :seller, :buyer, images_attributes: [:image_url]).merge(seller: current_user.id)
   end
 
+  
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  
+  def move_to_index
+    redirect_to action: :index unless user_signed_in? && current_user.id == @item.seller
+  end
 end
