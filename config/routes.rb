42a6ec76c7b246d558d5dc909d4bf2e37166dbad
kuromu0104 <nccjb457@yahoo.co.jp>
2020-07-15 18:47:35 +0900

    重複したルーティングの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'items/index'
   get "users/show"
   get "users/logout"
   get "users/credit_card"
+
   devise_for :users,  :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'
    }
-  resources :users
+  resources :users 
   root "items#index"
   resources :items do
     collection do
