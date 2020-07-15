9718caed5710f4250fd8135391fea73bb1b80f50
kuromu0104 <nccjb457@yahoo.co.jp>
2020-07-15 13:59:47 +0900

    マイページマークアップ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'items/index'
+  get "users/show"
   devise_for :users,  :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'
