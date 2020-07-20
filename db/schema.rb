c113726b9d2678c30961cba0e08df265d3b81bd8
eri-Practice <64237498+eri-Practice@users.noreply.github.com>
2020-07-20 11:01:37 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_17_012342) do
+ActiveRecord::Schema.define(version: 2020_07_09_073646) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image_url"
