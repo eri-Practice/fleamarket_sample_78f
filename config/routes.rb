75c6929d2f1ee8468abe4c16bf6b1ca3a1f76536
muro-chan <65319366+muro-chan@users.noreply.github.com>
2020-07-20 19:09:28 +0900

    出品ボタンの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  #get 'items/index'
+  devise_for :users,  :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+   }
+
+  resources :users, only: [:show, :index, :new, :create] do
+    collection do
+      get:logout
+      get:credit_card
+    end
+  end
+
   root "items#index"
   resources :items do
     collection do
