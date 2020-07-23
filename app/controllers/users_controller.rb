07b5b6354b3a9a59e56502deff09e9229b1efb5b
kuromu0104 <nccjb457@yahoo.co.jp>
2020-07-23 12:43:40 +0900

    新規登録エラーハンドリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to root_path, notice: '登録が完了しました。ログインしてください。'
     else
-      render :new
+      render :new, notice: '登録に失敗しました'
     end
   end
 
