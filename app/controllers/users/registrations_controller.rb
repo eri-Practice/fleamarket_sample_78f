6ef0c0ac057bd09ade7df7bd29b3401476c8d3e3
kuromu0104 <nccjb457@yahoo.co.jp>
2020-07-23 19:49:50 +0900

    Update registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -26,7 +26,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: '登録が完了しました。ログインしてください。'
+      redirect_to new_user_session_path, notice: '登録が完了しました。ログインしてください。'
     else
       render :new, notice: '登録に失敗しました'
     end
